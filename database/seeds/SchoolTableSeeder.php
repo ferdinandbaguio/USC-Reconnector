30d8451c3b5272e878bec1333d768e45498a43c5
Ferdinand Bryle Baguio <baguioferdinand@gmail.com>
2018-07-29 00:03:26 +0800

    asd

--- database/seeds/SchoolTableSeeder.php
+++ database/seeds/SchoolTableSeeder.php
@@ -19,9 +19,12 @@ class SchoolTableSeeder extends Seeder
 		    ->create()
 		    ->each(function($school) {
 			    $school->department()
-			           ->saveMany( factory(App\Department::class,2)->create(['school_id' => $school->id]));
+			           ->saveMany( factory(App\Department::class,2)
+			           ->create(['school_id' => $school->id]))
 			           ->each(function($department){
-			             $department->course()->saveMany(factory(App\Course::class,2)->create(['department_id' => $department->id]));
+			             $department->course()
+			             	->saveMany(factory(App\Course::class,2)
+			             	->create(['department_id' => $department->id]));
 			        	});
 			});
     }
