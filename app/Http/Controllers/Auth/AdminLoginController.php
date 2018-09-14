1b19653dc04575d5971f97f7bd44f50637f7c865
Ferdinand Bryle Baguio <baguioferdinand@gmail.com>
2018-09-14 20:31:31 +0800

    login changed

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -18,20 +18,20 @@ class AdminLoginController extends Controller
         'email'   => 'required|email',
         'password' => 'required|min:6'
       ]);
-      
+
       // Attempt to log the user in
-      if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
+      if (Auth::guard('admin')->attempt(['idnumber' => $request->idnumber, 'password' => $request->password], $request->remember)) {
         // if successful, then redirect to their intended location
-        return redirect()->intended(route('admin.dashboard'));
+        return redirect()->intended(route('admin'));
       } 
       // if unsuccessful, then redirect back to the login with the form data
-      return redirect()->back()->withInput($request->only('email', 'remember'));
+      return redirect()->back()->withInput($request->only('idnumber', 'remember'));
     }
 
     public function logout()
     {
         Auth::guard('admin')->logout();
-        return redirect('/admin');
+        return redirect('/');
     }
 
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,71 +1,48 @@
 @extends('layouts.app_login')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<!-- LOGIN MODAL start -->
+    <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg modalWidth" role="document">
+    <div class="modal-content modalBg"> 
+        <div class="modal-header mt-0 mb-0" style="border-bottom:0px;">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
         </div>
-    </div>
-</div>
+        <div class="modal-body">
+        <!-- Modal body start -->
+        <div class="row">
+        <div class="col-12 col-md mb-5">
+            <center>
+            <img src="//txt-dynamic.static.1001fonts.net/txt/dHRmLjMyLmZlZmJmYi5TbTl1WVhNZ1IzZGhjRzgsLjAA/blackchancery.regular.png" 
+            style="width: auto;">
+            <h1 class="signInHeader"> Sign In </h1>
+            <form autocomplete="off" action="{{route('admin.login.submit')}}" method="POST">
+                {{ csrf_field() }}
+                <div class="col-md-12">
+                    <span data-feather="user" class="logFeather"> </span>
+                    <input type="text" placeholder="ID Number" name="idnumber" id="loginInput">
+                </div>
+                <div class="col-md-12">
+                    <span data-feather="lock" class="logFeather"> </span>
+                    <input type="password" placeholder="Password" name="password" id="loginInput">
+                </div>
+                <div>
+                    <input type="submit" value="Login" id="loginButton" class="mt-5" ><br>
+                </div>
+            </form>
+            </center>
+        </div> <!-- col-12 col-md mb-5 -->
+        </div> <!-- row -->
+        </div> <!-- modal-body -->
+        <!-- Modal body end -->
+        <!--<div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+        </div> -->
+    </div> <!-- modal-content modalBg -->
+    </div> <!-- modal-dialog modal-lg modalWidth -->
+    </div> <!-- modal fade -->
+    <!-- LOGIN MODAL end  --> 
 @endsection
