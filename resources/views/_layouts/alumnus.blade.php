dda3f36c52aee9d50b10cb6604a4a5279400fa65
LecarreGavini <lecarregavini@gmail.com>
2018-09-20 02:38:13 +0800

    Moved layouts

--- resources/views/_layouts/alumnus.blade.php
+++ resources/views/_layouts/alumnus.blade.php
@@ -14,7 +14,7 @@
 </head>
 
 <body>    
-  @include('_layouts.navigation')
+  @include('_inc.navigation')
   <div class="container-fluid contentContainer">
   <div class="container">
       @include('_inc.messages')
