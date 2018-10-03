bbe0966b01631850f0553cdd1f563459a0ee70da
Jonas <gwapojonas24@gmail.com>
2018-10-03 10:07:17 +0800

    Updates

--- resources/views/newhome.blade.php
+++ resources/views/newhome.blade.php
@@ -60,9 +60,34 @@
     </div>
 
 
-    <div class="container-fluid" style="height: 200px;">
-      Jonas is not done :D
-    </div>
+    
+      <div id="carouselExampleIndicators" class="carousel slide w-50" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="/img/homepage_images/Pic5.jpg" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="/img/homepage_images/Pic6.jpg" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="/img/homepage_images/Pic7.jpg" alt="Third slide">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    
 
 
 
