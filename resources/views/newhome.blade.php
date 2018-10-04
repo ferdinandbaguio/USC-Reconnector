11c84a4eccdb5c4aa43ce853dee04ad5f16b0918
Jonas <gwapojonas24@gmail.com>
2018-10-04 23:05:44 +0800

    Modal Login with register is now usable

--- resources/views/newhome.blade.php
+++ resources/views/newhome.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" href="{{ asset('img/logo/Logo.ico') }}" type="image/gif" sizes="16x16">
+    <link rel="icon" href="{{ asset('img/logo/LogoBlue.png') }}" type="image/gif" sizes="16x16">
 
     <!-- Main bootstrap Core files -->
     <link rel="stylesheet" href="{{ asset('bootstrap/dist/css/bootstrap.min.css') }}">
@@ -27,29 +27,29 @@
     <!-- NAVIGATION BAR AND WELCOMING -->
     <div class="container-fluid SecondContainer p-0">
       <div class="container p-0 mt-3">
-        <nav class="navbar navbar-expand-md w-100" style="position:sticky">
+        <nav class="navbar navbar-expand-md w-100">
           <div class="container">
             <a class="navbar-brand mr-auto" href="/newhome">
               <img src="img/logo/studrec2.png" class="logoPic" alt="Logo">
             </a>
             
             <a class="navbar-brand ml-auto" href="#loginModal" data-toggle="modal">
-              <p class="mb-0 bellShake" style="color: #CECFD5; font-size: 17px;"><i class="fas fa-feather-alt"></i> Login/Sign In </p>
+              <p class="mb-0 bellShake loginBtn"><i class="fas fa-feather-alt"></i> Login/Sign In </p>
             </a>
 
           </div>
         </nav>
 
-        <div class="container-fluid mt-5">
-          <div class="row mt-auto">
-            <div class="col-12 mt-2">
-            <img src="/img/logo/Logo.png" class="d-block mx-auto" width="180px">
+        <div class="container-fluid mt-md-5">
+          <div class="row pt-xl-5">
+            <div class="col-12">
+            <img src="/img/logo/LogoBlue.png" class="d-block mx-auto" width="240px">
             </div>
           </div>
-          <div class="row">
+          <div class="row px-sm-2">
             <h1 class="display-4 text-white mx-auto text-center numCount">Welcome to Student-Reconnector!</h1>
           </div>
-          <div class="row mb-auto">
+          <div class="row">
             <button type="button" class="btn btn-outline-light ml-auto mr-2">Jonas Gwapo</button>
             <button type="button" class="btn btn-outline-light mr-auto">Join us now!</button>
           </div>
@@ -114,7 +114,7 @@
             <label class="display-3 text-white mx-auto counter"> <span class="count numCount">1029 </span></label>
           </div>
           <div class="row">
-            <div class="col-4 mx-auto pt-3" style="border-top:1px solid #8CC5CC">
+            <div class="col-4 mx-auto pt-3 borderBelowCount">
             <p class="text-white text-center numCount"> Accounts Approved </p>
             </div>
           </div>
@@ -125,7 +125,7 @@
             <label class="display-3 text-white mx-auto counter"> <span class="count numCount">800 </span></label>
           </div>
           <div class="row">
-            <div class="col-4 mx-auto pt-3" style="border-top:1px solid #8CC5CC">
+            <div class="col-4 mx-auto pt-3 borderBelowCount">
             <p class="text-white text-center numCount"> Alumni Tracked </p>
             </div>
           </div>
@@ -136,7 +136,7 @@
             <label class="display-3 text-white mx-auto counter"> <span class="count numCount">3029 </span></label>
           </div>
           <div class="row">
-            <div class="col-4 mx-auto pt-3" style="border-top:1px solid #8CC5CC">
+            <div class="col-4 mx-auto pt-3 borderBelowCount">
             <p class="text-white text-center numCount"> Job Offers </p>
             </div>
           </div>
@@ -147,7 +147,7 @@
             <label class="display-3 text-white mx-auto counter"> <span class="count numCount">329 </span></label>
           </div>
           <div class="row">
-            <div class="col-4 mx-auto pt-3" style="border-top:1px solid #8CC5CC">
+            <div class="col-4 mx-auto pt-3 borderBelowCount">
             <p class="text-white text-center numCount"> Announcements </p>
             </div>
           </div>
@@ -162,7 +162,7 @@
     <footer class="container-fluid pt-5 pb-2" style="background: url(/img/div_bgs/abg.jpg);background-size: cover;">
         <div class="row pb-5">
           <div class="col-12 col-md-3 mb-5">
-            <img src="img/logo/studrec1.png" class="d-block mx-auto" width="200px" alt="Logo">
+            <img src="img/logo/studrec3.png" class="d-block mx-auto" width="200px" alt="Logo">
           </div>
 
           <div class="col-6 col-md-3">
@@ -178,7 +178,7 @@
             <h3 class="footTxtColor"> Tools </h3>
             <ul class="list-unstyled">
               <li> <a href="" class="footTxtColor"> <i class="fas fa-angle-right"></i> Laravel </a> </li>
-              <li> <a href="" class="footTxtColor"> <i class="fas fa-angle-right"></i> jQuery </a> </li>
+              <li> <a href="" class="footTxtColor"> <i class="fas fa-angle-right"></i> JavaScript </a> </li>
               <li> <a href="" class="footTxtColor"> <i class="fas fa-angle-right"></i> Bootstrap 4 </a> </li>
             </ul>
           </div>
@@ -219,7 +219,7 @@
             <div class="d-block w-100" style="background: url(/img/homepage_images/Pic5.jpg);background-position: center;background-size: cover; height:400px">
             </div>
             <div class="carousel-caption d-none d-md-block">
-              <h5 style="text-shadow: 0 0 5px black">JONAS GWAPO</h5>
+              <h5>JONAS GWAPO</h5>
               <p>JONAS GWAPO</p>
             </div>
           </div>
@@ -227,7 +227,7 @@
             <div class="d-block w-100" style="background: url(/img/homepage_images/Pic6.jpg);background-position: center;background-size: cover; height:400px">
             </div>
             <div class="carousel-caption d-none d-md-block">
-              <h5 style="text-shadow: 0 0 5px black">JONAS GWAPO</h5>
+              <h5>JONAS GWAPO</h5>
               <p>JONAS GWAPO</p>
             </div>
           </div>
@@ -235,7 +235,7 @@
             <div class="w-100" style="background: url(/img/homepage_images/Pic7.jpg);background-position: center;background-size: cover; height:400px">
             </div>
             <div class="carousel-caption d-none d-md-block">
-              <h5 style="text-shadow: 0 0 10px black">JONAS GWAPO</h5>
+              <h5>JONAS GWAPO</h5>
               <p>JONAS GWAPO</p>
             </div>
           </div>
@@ -266,44 +266,41 @@
           <div class="modal-body">
             <div class="row pb-3">
               <div class="col-12">
-                <img src="img/logo/studrec1.png" class="logoPic d-block mx-auto" alt="Logo">
+                <img src="img/logo/studrec3.png" class="logoPic d-block mx-auto" alt="Logo">
               </div>
             </div>
-            
-
-            <div id="loginRegister" class="carousel slide w-100 mx-auto" data-ride="carousel">
-
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <form action="{{route('login.submit')}}" method="POST">
-                  {{ csrf_field() }}
-                <div class="row mt-4">
-                  <div class="col-md-8 mx-auto">
-                    <label class="m-0"> ID Number:</label>
-                    <input type="text" class="form-control" name="idnumber" placeholder="I.D Number"> 
-                  </div>
-                </div>
-                <div class="row mt-2">
-                  <div class="col-md-8 mx-auto">
-                    <label class="m-0"> Password:</label>
-                    <input type="password" class="form-control" name="password" placeholder="Password"> 
-                  </div>
-                </div>
-                <div class="row mt-4">
-                  <div class="col-md-8 mx-auto">
-                    <input type="submit" value="Login" class="btn w-100 text-white" style="background-color:#4E9DA6">
-                  </div>
-                </div>
-                <div class="row mb-4">
-                  <div class="col-md-8 mx-auto">
-                    <label>No Account Yet?</label> <a href="#loginRegister" role="button" data-slide="next"> Register Here </label>
-                  </div>
-                </div>
-                </form>
+            <!-- FORM FOR SIGNING IN -->
+            <div class="container-fluid" id="loginForm">
+            <form action="{{route('login.submit')}}" method="POST">
+              {{ csrf_field() }}
+            <div class="row mt-4">
+              <div class="col-md-8 mx-auto">
+                <label class="m-0"> ID Number:</label>
+                <input type="text" class="form-control" name="idnumber" placeholder="I.D Number"> 
               </div>
+            </div>
+            <div class="row mt-2">
+              <div class="col-md-8 mx-auto">
+                <label class="m-0"> Password:</label>
+                <input type="password" class="form-control" name="password" placeholder="Password"> 
+              </div>
+            </div>
+            <div class="row mt-4">
+              <div class="col-md-8 mx-auto">
+                <input type="submit" value="Login" class="btn w-100 text-white" style="background-color:#4E9DA6">
+              </div>
+            </div>
+            <div class="row mt-2 mb-4">
+              <div class="col-md-8 mx-auto">
+                <a href="#" id="regClick"> Register Here </a> <label> to create an account</label> 
+              </div>
+            </div>
+            </form>
+            </div>
+            <!-- FORM FOR SIGNING IN END-->
 
-
-          <div class="carousel-item">
+            <!-- FORM FOR Registering -->
+            <div class="container-fluid collapse" id="regForm">
             <form action="" method="POST">
               <div class="row mt-4">
                 <div class="col-md-8 mx-auto">
@@ -317,83 +314,49 @@
                   <input type="password" class="form-control" placeholder="Password"> 
                 </div>
               </div>
-              <div class="row mt-4 mb-4">
+              <div class="row mt-2">
+                <div class="col-md-8 mx-auto">
+                  <label class="m-0"> Test INput:</label>
+                  <input type="text" class="form-control" placeholder="Testing Input only"> 
+                </div>
+              </div>
+              <div class="row mt-4 mb-2">
                 <div class="col-md-8 mx-auto">
                   <input type="submit" value="Register" class="btn w-100 text-white" style="background-color:#4E9DA6">
                 </div>
               </div>
+              <div class="row mb-4">
+                <div class="col-md-8 mx-auto">
+                  <input type="button" value="Back to Login" class="btn btn-danger w-100" id="logClick">
+                </div>
+              </div>
             </form>
-          </div>
-        </div>
-
-        <a class="carousel-control-prev" href="#loginRegister" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#loginRegister" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-    </div>
-
-
+            </div>
+            <!-- FORM FOR Registering END-->
           </div>
 
         </div>
       </div>
     </div>
-    
-
-<script type="text/javascript">
-  function showRegF(){
-    document.getElementById("loginForm").style.height = "0px";
-    document.getElementById("regForm").style.height = "auto";
-  }
-</script>
+  
 
 
 
-
-<!-- --------------------- -->
-
-<!-- --------------------------- -->
+  
 
 
-
-
-
-
-
-
-
-
-
-
-
-  <!-- Optional JavaScript -->
-
-
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS, then Customized JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script
   src="http://code.jquery.com/jquery-2.1.3.min.js"
   integrity="sha256-ivk71nXhz9nsyFDoYoGf2sbjrR9ddh+XDkCcfZxjvcM="
   crossorigin="anonymous"></script>
-  <script type="text/javascript">
-    $('.count').each(function () {
-    $(this).prop('Counter',0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 5000,
-            easing: 'swing',
-            step: function (now) {
-                $(this).text(Math.ceil(now));
-            }
-        });
-    });
-    </script>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  
+  
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
+  <!-- Customized JavaScript MUST BE IN THE END-->
+  <script src="js/newhome.js"></script>
   </body>
   </html>
