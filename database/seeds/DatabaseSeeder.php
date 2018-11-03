3defb7e9dd038c3b29b159413932ef3a5523bc2b
LecarreGavini <lecarregavini@gmail.com>
2018-11-03 14:39:16 +0800

    Made Buggy Seed and Fixed Migrations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -8,6 +8,13 @@ class DatabaseSeeder extends Seeder
     {
 
         factory(App\Models\User::class,100)->create();
+        factory(App\Models\Subject::class,20)->create();
+        factory(App\Models\Group_Class::class,15)->create();
+        factory(App\Models\Year::class,5)->create();
+        factory(App\Models\Semester::class,10)->create();
+        factory(App\Models\Schedule::class,15)->create();
+        factory(App\Models\Group_Schedule::class,20)->create();
+        factory(App\Models\Student_Class::class,150)->create();
         // $this->call(SchoolTableSeeder::class);
         // $this->call(UserTableSeeder::class);
 
